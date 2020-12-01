bd7849e6c8b2de170181c8e629a3006129cfa52a
Alexandria Robertson <67127042+alexemrob@users.noreply.github.com>
2020-12-01 10:54:34 -0600

    add footer component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar"
 import AboutMe from "./components/AboutMe"
 import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 function App() {
 
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/Contact" component={Contact} />
         </Switch>
       </Wrapper>
+      <Footer />
     </Router>
   );
 }
